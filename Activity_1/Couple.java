56742b2468435557e9cd6cb5bd6aa28a821bce1f
eL1833 <carmelamaejbenson@gmail.com>
2026-02-01 22:05:59 +0800

    Create Couple.java

---
+++ Activity_1/Couple.java
@@ -0,0 +1,19 @@
+package Activities;
+
+public class Couple {
+    private Person bride;
+    private Person groom;
+
+    public Couple(Person bride, Person groom){
+        this.bride = bride;
+        this.groom = groom;
+    }
+
+    public Person getBride(){
+        return bride;
+    }
+
+    public Person getGroom(){
+        return groom;
+    }
+}
