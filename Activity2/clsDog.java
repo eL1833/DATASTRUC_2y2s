79863fb216c6b6e4c89a2d131cc04472d9f4e5f5
eL1833 <carmelamaejbenson@gmail.com>
2026-02-03 22:03:00 +0800

    Create clsDog.java

---
+++ Activity2/clsDog.java
@@ -0,0 +1,18 @@
+package DATASTRUC_Activities.Activity2;
+
+public class clsDog extends clsAnimalKingdom{
+
+     public clsDog(){
+        super();
+    }
+
+     //subclass constructor
+    public clsDog(String n, String h, String c, int a){
+        super(n, h, c, a);
+    }
+
+    public void displayInfo(){
+        System.out.println("The Dog's name is "+super.getName()+", it lives in the "+super.getHabitat()+", with a color of "+super.getColor()+" and its age is "+super.getAge());
+    }
+
+}
