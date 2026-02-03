4c920ac05b8b856884d02bdc28dad8a1e4e872e2
eL1833 <carmelamaejbenson@gmail.com>
2026-02-03 22:03:24 +0800

    Create clsParrot.java

---
+++ Activity2/clsParrot.java
@@ -0,0 +1,19 @@
+package DATASTRUC_Activities.Activity2;
+
+public class clsParrot extends clsAnimalKingdom{
+
+     public clsParrot(){
+        super();
+    }
+
+     //subclass constructor
+    public clsParrot(String n, String h, String c, int a){
+        super(n, h, c, a);
+    }
+
+    public void displayInfo(){
+        System.out.println("The Parrot's name is "+super.getName()+", it lives in the "+super.getHabitat()+", with a color of "+super.getColor()+" and its age is "+super.getAge());
+    }
+
+}
+
