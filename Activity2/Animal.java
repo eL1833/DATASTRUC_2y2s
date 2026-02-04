71a850756d12fdd40a8927dfd89db40a87b510ac
eL1833 <carmelamaejbenson@gmail.com>
2026-02-04 22:27:33 +0800

    Update Animal.java

--- Activity2/Animal.java
+++ Activity2/Animal.java
@@ -3,10 +3,13 @@ package DATASTRUC_Activities.Activity2;
 public class Animal {
     
     public static void main(String[] args) {
-        
-        clsCat c = new clsCat("Mario", "House", "Orange", 3);
-        clsDog d = new clsDog("Rocky", "House", "Brown", 4);
-        clsParrot p = new clsParrot("Chie", "House", "Red", 5);
+        clsCat c; //these variable names are created to hold the memory address of the concrete object,
+        clsDog d; //so although, a reference to an abstract superclass object is not concrete,
+        clsParrot p;; //you can store a concrete subclass object reference there.
+        c = new clsCat("Mario", "House", "Orange", 3);
+        d = new clsDog("Rocky", "House", "Brown", 4);
+        p = new clsParrot("Chie", "House", "Red", 5);
+
         c.displayInfo();
         d.displayInfo();
         p.displayInfo();
