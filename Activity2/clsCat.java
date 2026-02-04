af05e0f9797bd621d31e97d7b7d979856328430f
eL1833 <carmelamaejbenson@gmail.com>
2026-02-04 22:28:17 +0800

    Update clsCat.java

--- Activity2/clsCat.java
+++ Activity2/clsCat.java
@@ -12,6 +12,7 @@ public class clsCat extends clsAnimalKingdom {
         super(n, h, c, a);
     }
 
+    //every subclass must contain the abstract method of the parent class
     public void displayInfo(){
         System.out.println("The Cat's name is "+super.getName()+", it lives in the "+super.getHabitat()+", with a color of "+super.getColor()+" and its age is "+super.getAge());
     }
