a4fe7f40ae8415a0665658a52d8d7d9e1200cd86
eL1833 <carmelamaejbenson@gmail.com>
2026-02-04 22:28:39 +0800

    Update clsCat.java

--- Activity2/clsCat.java
+++ Activity2/clsCat.java
@@ -1,20 +1,19 @@
 package DATASTRUC_Activities.Activity2;
 
-public class clsCat extends clsAnimalKingdom {
-    
-    //subclass default constructor
-    public clsCat(){
+public class clsDog extends clsAnimalKingdom{
+
+     public clsDog(){
         super();
     }
 
      //subclass constructor
-    public clsCat(String n, String h, String c, int a){
+    public clsDog(String n, String h, String c, int a){
         super(n, h, c, a);
     }
 
     //every subclass must contain the abstract method of the parent class
     public void displayInfo(){
-        System.out.println("The Cat's name is "+super.getName()+", it lives in the "+super.getHabitat()+", with a color of "+super.getColor()+" and its age is "+super.getAge());
+        System.out.println("The Dog's name is "+super.getName()+", it lives in the "+super.getHabitat()+", with a color of "+super.getColor()+" and its age is "+super.getAge());
     }
 
 }
