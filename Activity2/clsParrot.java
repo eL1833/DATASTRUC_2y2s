0b326068fabcb3d2705b65c1180801450c48a374
eL1833 <carmelamaejbenson@gmail.com>
2026-02-04 22:29:19 +0800

    Update clsParrot.java

--- Activity2/clsParrot.java
+++ Activity2/clsParrot.java
@@ -11,6 +11,7 @@ public class clsParrot extends clsAnimalKingdom{
         super(n, h, c, a);
     }
 
+    //every subclass must contain the abstract method of the parent class
     public void displayInfo(){
         System.out.println("The Parrot's name is "+super.getName()+", it lives in the "+super.getHabitat()+", with a color of "+super.getColor()+" and its age is "+super.getAge());
     }
