7d897c04aa34c0b70198f6e461e95376e24cc990
eL1833 <carmelamaejbenson@gmail.com>
2026-02-12 10:28:53 +0800

    Create Person.java

---
+++ Activity3/Person.java
@@ -0,0 +1,14 @@
+public abstract class Person{
+
+   public abstract void displayInfo();
+   
+   protected String lastname;
+   protected String firstname;
+   
+   public void getLastName(){
+      return String lastname;
+   }
+   public void getFirstName(){
+      return String firstname;
+   }
+}
