3e695c655c365a23604cc1a406932e94e78d3279
eL1833 <carmelamaejbenson@gmail.com>
2026-02-17 21:52:30 +0800

    Create RegularEmployee.java

---
+++ Activity3/RegularEmployee.java
@@ -0,0 +1,24 @@
+package DATASTRUC_Activities.Activity2;
+
+public class RegularEmployee extends Employee {
+    
+    public double bonus;
+
+    public RegularEmployee(String firstname, String lastname, String idNo, double hoursWrk, double hourlyRate, String position, String department, double bonus){
+      super(firstname, lastname, idNo, hoursWrk, hourlyRate, position, department);
+      this.bonus = bonus;
+    }
+
+    public double getBonus(){
+        return bonus;
+    }
+
+    public void setBonus(double bonus){
+        this.bonus = bonus;
+    }
+
+    @Override 
+    public double iComputeSalary(){
+        return (getHoursWrk() * getHourlyRate()+bonus);
+    }
+}
