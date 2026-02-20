cdbd9329c21dd6d8a4c36e44406ebe10fd1a39d7
eL1833 <carmelamaejbenson@gmail.com>
2026-02-20 08:58:21 +0800

    Update TestPerson.java

--- Activity3/TestPerson.java
+++ Activity3/TestPerson.java
@@ -5,11 +5,11 @@ public class TestPerson {
     public static void main(String[] args) {
         
         RegularEmployee regularEmployee = new RegularEmployee("Christina", "Benson", 
-                                                                "122787", 8.0, 500, 
+                                                                "122787", 8.0, 80, 
                                                                 "Accountant", "Finance", 300.0);
         ContractualEmployee contractualEmployee = new ContractualEmployee("Danica", "Putan", 
-                                                                "120604", 8.0, 400,
-                                                                "Student Teacher", "Math", 100.0);
+                                                                "120604", 9.0, 90,
+                                                                "Student Teacher", "Math", 1.25);//overtime per hour
         Student student = new Student("Leo", "Benson", 
                                         "042313", "Civil Engineering", "Engineering");  
                                         
