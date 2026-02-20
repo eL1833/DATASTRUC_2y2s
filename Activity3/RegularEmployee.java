4760d4c532ec286c639f28679acd51bf2dff6209
eL1833 <carmelamaejbenson@gmail.com>
2026-02-20 08:56:35 +0800

    Update RegularEmployee.java

--- Activity3/RegularEmployee.java
+++ Activity3/RegularEmployee.java
@@ -3,7 +3,7 @@ package DATASTRUC_Activities.Activity2;
 public class RegularEmployee extends Employee {
     
     public double bonus;
-
+    
     public RegularEmployee(String firstname, String lastname, String idNo, double hoursWrk, double hourlyRate, String position, String department, double bonus){
       super(firstname, lastname, idNo, hoursWrk, hourlyRate, position, department);
       this.bonus = bonus;
