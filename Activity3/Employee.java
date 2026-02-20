8699beabe7679522dcf5fe0cb48263a79e58cf1c
eL1833 <carmelamaejbenson@gmail.com>
2026-02-20 08:55:17 +0800

    Update Employee.java

--- Activity3/Employee.java
+++ Activity3/Employee.java
@@ -1,35 +1,58 @@
 package DATASTRUC_Activities.Activity2;
 
-public abstract class Person{
+public class Employee extends Person implements iCompute{
    
-   protected String lastname;
-   protected String firstname;
+   private double hoursWrk;
+   private double hourlyRate;
+   private String idNo;
+   private String position;
+   private String department;
 
-   public Person(){
-      lastname = " ";
-      firstname = " ";
-   }
-
-   public Person(String firstname, String lastname){
-      this.firstname = firstname;
-      this.lastname = lastname;
+   public Employee(String firstname, String lastname, String idNo, double hoursWrk, double hourlyRate, String position, String department){
+      super(firstname, lastname);
+      this.idNo = idNo;
+      this.hoursWrk = hoursWrk;
+      this.hourlyRate = hourlyRate;
+      this.position = position;
+      this.department = department;
    }
    
-   public String getLastName(){
-      return lastname;
+   public String getIdNo(){
+      return idNo;
    }
-   public String getFirstName(){
-      return firstname;
+   public String getPosition(){
+      return position;
+   }
+   public String getDepartment(){
+      return department;
+   }
+   public double getHoursWrk(){
+      return hoursWrk;
+   }
+   public double getHourlyRate(){
+      return hourlyRate;
+   }
+   
+   public void setIdNo(String idNo){
+      this.idNo = idNo;
+   }
+   public void setPosition(String position){
+      this.position = position;
+   }
+   public void setDepartment(String department){
+      this.department = department;
+   }
+   public void setHoursWrk(double hoursWrk){
+      this.hoursWrk = hoursWrk;
+   }
+   public void setHourlyRate(double hourlyRate){
+      this.hourlyRate = hourlyRate;
    }
 
-   public void setLastName(String lastname){
-      this.lastname = lastname;
+   @Override
+   public double iComputeSalary(){
+      return (getHoursWrk() * getHourlyRate());
    }
-   public void setFirstName(String firstname){
-      this.firstname = firstname;
-   }
-
-   public String displayInfo(){
-      return " "+lastname+", "+firstname;
-   };
+   
 }
+  
