ca830a632c35a00bd92c845f5fbf20f6fe19d1ea
eL1833 <carmelamaejbenson@gmail.com>
2026-03-11 23:43:28 +0800

    Update Activity4.java

--- Activity4.java
+++ Activity4.java
@@ -1,48 +1,53 @@
-import java.util.*;
+package DATASTRUC_Activities;
 
 class Benson_gradeBook {
 
    public static void main(String[] args){
    
-   int[][] scores = {{87, 96, 70},
-                    {68, 87, 90},
-                    {94, 100, 90},
-                    {100, 81, 82},
-                    {83, 65, 85},
-                    {78, 87, 65},
-                    {85, 75, 83},
-                    {91, 94, 100},
-                    {76, 72, 84},
-                    {87, 93, 73}};
-   
-   int lowestGrade = scores[0][0];
-   int highestGrade = scores[0][0];
-                    
-   System.out.println("Welcome to the grade book for \n CS101 Introduction to C++ Programming!");
-   System.out.println();
-   System.out.println("The grades are:");
-   System.out.println();
-   System.out.println("Student    Test 1  Test 2  Test 3  Average");
-   
-   for(int i = 0; i < scores.length; i++){
-      int sum = 0;
-      double average;
-      System.out.print("Student "+(i+1)+"    ");
-      
-      for(int j = 0; j < scores[i].length; j++){
-         
-         System.out.print(scores[i][j]+"     ");
-         
-         sum = sum + scores[i][j];
-         average = sum/3.0;
-         
-         System.out.println(average);
-      } 
-    
-   }   
-   
-   
-  
-                     
-   }
+        //store sa array and scores
+        int[][] scores = {
+                {87, 96, 70},
+                {68, 87, 90},
+                {94, 100, 90},
+                {100, 81, 82},
+                {83, 65, 85},
+                {78, 87, 65},
+                {85, 75, 83},
+                {91, 94, 100},
+                {76, 72, 84},
+                {87, 93, 73}};
+
+        //initialize values sa lowest and highest
+        int lowest = scores[0][0] ;
+        int highest = scores[0][0];
+                            
+        System.out.println("Welcome to the grade book for \nCS101 Introduction to C++ Programming!");
+        System.out.println();
+        System.out.println("The grades are:");
+        System.out.println();
+        System.out.println("Student   Test1  Test2  Test3  Average");
+        
+        //i-loop una ang student row, then ang test column dayon
+        for(int i = 0; i < scores.length; i++){
+
+            int sum = 0;
+            System.out.print("Student "+(i+1)+"   ");
+
+            for(int j = 0; j < scores[i].length; j++){
+
+                System.out.print(scores[i][j]+"    ");
+                lowest = Math.min(lowest, scores[i][j]);
+                highest = Math.max(highest, scores[i][j]);
+                sum += scores[i][j];
+            }
+            
+            double average = sum/ 3.0;
+            System.out.printf("%.2f",average);
+            System.out.println();
+        }
+        
+        System.out.print("\nLowest grade in the grade book is "+lowest+"\n"
+                        +"Highest grade in the grade book is "+highest
+        );
+    }
 }
