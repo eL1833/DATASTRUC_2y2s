b097703640f25c4f06a3aa2266fc7c92903e358f
eL1833 <carmelamaejbenson@gmail.com>
2026-04-06 22:04:17 +0800

    Update MainReservation.java

--- Reservation/MainReservation.java
+++ Reservation/MainReservation.java
@@ -1,15 +1,14 @@
 package DATASTRUC_Activities.Reservation;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class MainReservation {
 
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        Stack<Reservation> stack = new Stack<>();
-        int iResNum = 1;
         int choice;
+        int resNum = 1;
 
         do {
             System.out.println("\n=== Reservation System ===");
@@ -17,7 +16,7 @@ public class MainReservation {
             System.out.println("2. Cancel Reservation");
             System.out.println("3. Display All Reservation");
             System.out.println("4. Exit");
-            System.out.print("\nEnter your choice: ");
+            System.out.print("Enter option: ");
 
             choice = input.nextInt();
             input.nextLine();
@@ -25,37 +24,38 @@ public class MainReservation {
             switch (choice) {
 
                 case 1:
-                    int resnum = iResNum++;
-
-                    System.out.println("\n=== Add Reservation ===");
-                    System.out.println("Reservation Number: " + resnum);
-
-                    System.out.print("Enter Name: ");
+                    System.out.println("Reservation No.: "+resNum);
+                
+                    System.out.print("Enter name: ");
                     String name = input.nextLine();
 
-                    System.out.print("Enter Date: ");
+                    System.out.print("Enter date: ");
                     String date = input.nextLine();
 
                     System.out.print("Enter number of persons: ");
                     int numPer = input.nextInt();
                     input.nextLine();
 
-                    Reservation reservation =
-                            new Reservation(resnum, name, date, numPer);
+                    Reservation r = new Reservation(resNum, name, date, numPer);
+                    resNum++;
 
-                    Reservation.addReservation(stack, reservation);
+                    Reservation.push(r);
                     break;
 
                 case 2:
-                    Reservation.cancelReservation(stack);
+                    Reservation removed = Reservation.pop();
+
+                    if (removed != null) {
+                        System.out.println("Reservation of " + removed.name + " is Cancelled.");
+                    }
                     break;
 
                 case 3:
-                    Reservation.displayAll(stack);
+                    Reservation.displayAll();
                     break;
 
                 case 4:
-                    System.out.println("Exiting program...");
+                    System.out.println("Goodbye!");
                     break;
 
                 default:
