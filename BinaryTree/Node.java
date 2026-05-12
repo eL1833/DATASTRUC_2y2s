d37ebd683a9cf7ab665cacc87aa99922a1aaec91
eL1833 <carmelamaejbenson@gmail.com>
2026-05-13 07:11:16 +0800

    Update Node.java

--- BinaryTree/Node.java
+++ BinaryTree/Node.java
@@ -1,30 +1,31 @@
-public class Node {
+class Node {
+        private int data;
+        private Node left;
+        private Node right;
 
-    private int data;
-    private Node left;
-    private Node right;
+public Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
 
-    public Node(int data) {
-        this.data = data;//when node is created, dre ma store ang value
-        left = null;//initialized to empty
-        right = null;
-    }
-    
-    public int getData() {
-        return data;
-    }
-    public Node getLeft() {
-        return left;
-    }
-    public Node getRight() {
-        return right;
-    }
+        public int getData() {
+            return data;
+        }
 
+        public Node getLeft() {
+            return left;
+        }
 
-    public void setLeft(Node left) {
-        this.left = left;
+        public Node getRight() {
+            return right;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
     }
-    public void setRight(Node right) {
-        this.right = right;
-    }
-}
