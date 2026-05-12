0be08325ec54d591b3c2ff94301e5ce1ebe7a8e0
eL1833 <carmelamaejbenson@gmail.com>
2026-05-13 07:17:52 +0800

    Update BinaryTree.java

--- BinaryTree/BinaryTree.java
+++ BinaryTree/BinaryTree.java
@@ -1,45 +1,29 @@
 import java.util.Scanner;
-
 public class BinaryTree {
-
+    
     private Node root;
-
+    
     public BinaryTree() {
-        root = null;//initialized root to empty
-    }
-
-    public Node getRoot() {
-        return root;
+        root = null;
     }
 
     public Node createNode(int value) {
         return new Node(value);
     }
-    
-    
-//add
-      public void getInput() {
-   
-         Scanner sc = new Scanner(System.in);
-   
-         System.out.print("How many values to insert? ");
-         int n = sc.nextInt();
-   
-         for (int i = 1; i <= n; i++) {
-   
-          System.out.print("Enter value " + i + ": ");
-          int value = sc.nextInt();
-   
-          insert(value);//proceed to insert method
-          }
-       }
 
+    public void getInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many values to insert? ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Enter value " + i + ": ");
+            int value = sc.nextInt();
+            insert(value);
+        }
+    }
 
-//insert 
     public void insert(int value) {
-
         Node newNode = createNode(value);
-
         if (root == null) {
             root = newNode;
             return;
@@ -47,69 +31,53 @@ public class BinaryTree {
 
         Node current = root;
         Node parent = null;
-
         while (current != null) {
-
             parent = current;
-
             if (value < current.getData()) {
                 current = current.getLeft();
             }
-
             else if (value > current.getData()) {
                 current = current.getRight();
             }
-
             else {
                 System.out.println("Duplicate value not allowed.");
                 return;
             }
         }
 
+
         if (value < parent.getData()) {
             parent.setLeft(newNode);
         }
-
         else {
             parent.setRight(newNode);
         }
     }
 
-//search   
     public void searchValue(int value) {
-
         Node current = root;
         String path = "";
-
         while (current != null) {
-
             if (current.getData() == value) {
-
                 if (path.equals("")) {
                     System.out.println(value + " is found at the ROOT.");
                 } else {
                     System.out.println(value + " is found.");
                     System.out.println("Path: " + path);
                 }
-
                 return;
             }
-
+            
             if (value < current.getData()) {
-
                 current = current.getLeft();
-
                 if (path.equals("")) {
                     path = "Left";
                 } else {
                     path += " -> Left";
                 }
             }
-
             else {
-
                 current = current.getRight();
-
                 if (path.equals("")) {
                     path = "Right";
                 } else {
@@ -117,26 +85,35 @@ public class BinaryTree {
                 }
             }
         }
-
         System.out.println(value + " is NOT found in the tree.");
     }
 
     public void displayTree() {
-
-        System.out.println("\nTree Elements (Preorder Traversal):");
-
-        preorder(root);
-
+        if (root == null) {
+            System.out.println("Tree is empty.");
+            return;
+        }
+        System.out.println("\n===== TREE DISPLAY =====");
+        System.out.println("Root: " + root.getData());
+        System.out.print("Left: ");
+        displayLeft(root.getLeft());
+        System.out.print("\nRight: ");
+        displayRight(root.getRight());
         System.out.println();
     }
-
-    private void preorder(Node node) {
-
+    
+    private void displayLeft(Node node) {
         if (node != null) {
-
             System.out.print(node.getData() + " ");
-            preorder(node.getLeft());
-            preorder(node.getRight());
+            displayLeft(node.getLeft());
+            displayLeft(node.getRight());
         }
     }
-}
+
+    private void displayRight(Node node) {
+        if (node != null) {
+            System.out.print(node.getData() + " ");
+            displayRight(node.getLeft());
+            displayRight(node.getRight());
+        }
+    }
