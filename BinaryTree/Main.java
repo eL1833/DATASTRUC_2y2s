da7fed292d5b573736452daf3b4f0c35e827a653
eL1833 <carmelamaejbenson@gmail.com>
2026-05-12 10:23:09 +0800

    Update Main.java

--- BinaryTree/Main.java
+++ BinaryTree/Main.java
@@ -1,4 +1,3 @@
-// Main.java
 import java.util.Scanner;
 
 public class Main {
@@ -9,17 +8,55 @@ public class Main {
 
         BinaryTree tree = new BinaryTree();
 
-        // Input values
-        tree.getInput();
+        int choice;
 
-        // Display tree
-        tree.displayTree();
+        do {
 
-        // Search value
-        System.out.print("\nEnter value to search: ");
-        int search = sc.nextInt();
+            System.out.println("\n===== BINARY SEARCH TREE MENU =====");
+            System.out.println("1. Insert Values");
+            System.out.println("2. Display Tree");
+            System.out.println("3. Search Value");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
 
-        // Search node
-        tree.searchValue(search);
+            choice = sc.nextInt();
+
+            switch (choice) {
+
+                case 1:
+
+                    // Insert values
+                    tree.getInput();
+                    break;
+
+                case 2:
+
+                    // Display tree
+                    tree.displayTree();
+                    break;
+
+                case 3:
+
+                    // Search value
+                    System.out.print("Enter value to search: ");
+                    int search = sc.nextInt();
+
+                    tree.searchValue(search);
+                    break;
+
+                case 4:
+
+                    // Exit program
+                    System.out.println("Program terminated.");
+                    break;
+
+                default:
+
+                    System.out.println("Invalid choice. Try again.");
+            }
+
+        } while (choice != 4);
+
+        sc.close();
     }
 }
